b77967c84420b1de9214969451102df5a36735ca
Singh543214 <princesingh88355@gmail.com>
2025-09-20 14:52:56 +0530

    Script-commited

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Sidebar.css";
 
-const Sidebar = ({ items }) => {
+const Sidebar = ({ items,updateRecords }) => {
   return (
     <div className="sidebar">
       {items.map((item, i) => (
-        <button key={i} className="sidebar-btn">
+        <button key={i} className="sidebar-btn" onClick={()=>updateRecords(i)}>
           {item}
         </button>
       ))}
